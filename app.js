e398a50016ca888959c86ca8f024e498185c5560
trfatihcinar <rea.fatihcinar@gmail.com>
2021-03-01 12:50:46 +0300

    bug fixed on pug and pug dynamic page render

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = 3003;
 
-app.set("view-engine", "pug");
+app.set("view engine", "pug");
 // say that we want to use pug template engine
 app.set("views", "views");
 // means that our pug views is located in views folder
@@ -15,8 +15,14 @@ const server = app.listen(PORT, () => {
 });
 
 app.get("/", (request, response, next) => {
+
+    var payload = {
+        pageTitle: "Twitter Clone",
+        pageHeading : "Welcome to Twitter, a Connected World",
+        pageDescription : "This is where the World talks to each other."
+    };
     
     const OKAY = 200;
     //response.status(OKAY).send("Server is ON! :)");
-    response.status(OKAY).render("home");
+    response.status(OKAY).render("home", payload);
 });
