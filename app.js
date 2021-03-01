d389fddd111fe0129e94ceed65f3d250f1103e3d
trfatihcinar <rea.fatihcinar@gmail.com>
2021-03-01 12:41:39 +0300

    setting up pug template

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 
 const PORT = 3003;
 
+app.set("view-engine", "pug");
+// say that we want to use pug template engine
+app.set("views", "views");
+// means that our pug views is located in views folder
+
 const server = app.listen(PORT, () => {
     console.log("Server is working on PORT " + PORT);
 });
@@ -12,5 +17,6 @@ const server = app.listen(PORT, () => {
 app.get("/", (request, response, next) => {
     
     const OKAY = 200;
-    response.status(OKAY).send("Server is ON! :)");
+    //response.status(OKAY).send("Server is ON! :)");
+    response.status(OKAY).render("home");
 });
