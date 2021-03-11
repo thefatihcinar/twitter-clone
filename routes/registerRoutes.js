ba228e8c1da8f754bb54466a5ca1c43d2d30877d
trfatihcinar <rea.fatihcinar@gmail.com>
2021-03-11 19:32:43 +0300

    checking whether email or username already in use

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -4,6 +4,9 @@ const express = require("express");
 // make an instance of express
 const app = express();
 
+const User = require("../schemas/UserSchema");
+// access to the User collection (table)
+
 // Use Body Parser
 // to parse elements in Request Form
 const bodyParser = require("body-parser");
@@ -30,7 +33,7 @@ router.get("/", (request, response, next) => {
 
 } );
 
-router.post("/", (request, response, next) => {
+router.post("/", async (request, response, next) => {
     // This function will evaluate post requests to the server or router
     const OKAY = 200;
 
@@ -45,6 +48,43 @@ router.post("/", (request, response, next) => {
 
     if(name && username && email && password){
         // there is no problem
+
+        // Gotta CHECK WHETHER THIS USERNAME OR EMAIL ALREADY EXISTS
+        let user = await User.findOne({
+            $or: [
+                {username: username}, 
+                {email: email}
+        ]})
+        .catch( (error) => {
+            console.log(error); // First log the error
+            // Then inform the user that an error occurred
+            payload.errorMessage = "oops something went wrong";
+            response.status(OKAY).render("register", payload);
+        })
+
+        // Now control whether there is a user found or not
+
+        if(user == null){
+            // there is no user with this username and email
+            // INSERT THE USER TO THE DATABASE
+        }
+        else{
+            // there is a user that  has this username or this email
+            // check which one is it
+            if( email == user.email){
+                // emails match
+                payload.errorMessage("Email already in use!");
+            }
+            else if ( username == user.username){
+                // usernames match
+                payload.errorMessage("Username already in use!");
+            }
+
+            response.status(200).render("register", payload);
+        }
+        
+      
+
         response.send("everything is cool for now");
     }
     else{
