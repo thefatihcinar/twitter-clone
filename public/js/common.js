2f72a7e867df75cd65683d0e7f40d8361c555af7
trfatihcinar <rea.fatihcinar@gmail.com>
2021-03-24 18:19:16 +0300

    getting the tweet entered with jquery

---
+++ public/js/common.js
@@ -0,0 +1,11 @@
+// This javascript file will be used for common things between pages
+
+// Getting the text from tweeting area with jQuery
+$("#tweetTextarea").keyup((event) => {
+
+    let textarea = $(event.target); // where event happens
+    let value = textarea.val().trim(); // read the value
+    
+    
+
+})
