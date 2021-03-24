c08a63dcbf5c871974d77f535bc31d1e7694e134
trfatihcinar <rea.fatihcinar@gmail.com>
2021-03-24 18:29:42 +0300

    enabling tweet button when tweet entered

--- public/js/common.js
+++ public/js/common.js
@@ -6,6 +6,30 @@ $("#tweetTextarea").keyup((event) => {
     let textarea = $(event.target); // where event happens
     let value = textarea.val().trim(); // read the value
     
+    // When the user enters a tweet
+    // Unless it is an empty tweet
+    // Enable the tweet button
+
+    let tweetButton = $("#tweetButton");
     
+    // might be null
+    if(tweetButton.length == 0){
+        
+        return console.error("tweet button not found");
+    }
+    
+    // button works
+    if(value == ""){
+        // if the tweet is an empty tweet
+        tweetButton.prop("disabled", true); 
+        // keep the button disabled
+        return;
+    }
+
+    // if the tweet is not empty
+    // normal tweet
+    // make the button enabled
+    tweetButton.prop("disabled", false);
+    return;
 
 })
