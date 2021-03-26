8bfe9e7b962ccd1c97e7758bd3da2a7d44c8328b
trfatihcinar <rea.fatihcinar@gmail.com>
2021-03-27 02:08:13 +0300

    render all incoming tweets in home page

--- public/js/home.js
+++ public/js/home.js
@@ -10,6 +10,42 @@ $(document).ready(() => {
     */
     // get the tweets
     $.get('/api/posts', (tweets) => {
-        console.log(tweets)
+        // render these tweets
+        renderTweets(tweets, $(".postsContainer"));
+
     })
 })
+
+function renderTweets(tweets, container){
+    /*
+        this function will render tweets that are coming from server
+        to the front-end in a way that is desired
+    */
+
+    // First flush the container
+    container.html(""); // make it empty
+
+    // There might be NO TWEETS 
+
+    if(tweets.length == 0){
+        // no tweets
+        const NO_TWEETS_TEXT = "nothing to show";
+        container.append(`<span class = 'nothingToShow'>${NO_TWEETS_TEXT}</span>`);
+        return;
+    }
+
+    // There are tweets
+    // render tweets one by one
+
+    tweets.forEach( tweet => {
+        
+        let renderedHTML = createTweetHTML(tweet);
+        // get the HTML code
+        container.append(renderedHTML);
+        // append this to the container
+    });
+    return;
+
+
+    
+}
